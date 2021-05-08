1d88415952ed39ddbb01236c3e0091c5c9502ed6
davmarksman <david.ike.mark@gmail.com>
2021-05-08 14:34:04 +0100

    Comments

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,47 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld :msg="headingName + ' Igloo'"/> -->
+    <button @click="lodashTest">{{btnName}}</button>
+    <button :class="myClass" @click="changeTitle">ChangeTitle</button>
+    <Avatar :username="headingName + ' Igloo'"></Avatar>
+    <CommentHolder></CommentHolder>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import lodash from 'lodash'
+import Avatar from 'vue-avatar'
+import CommentHolder from './components/CommentHolder'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+    Avatar,
+    CommentHolder
+  },
+  data() {
+    return {
+      btnName: "Lodash Other",
+      headingName: "Hello World",
+      myClass: "btn-red"
+    }
+  },
+  methods: {
+    lodashTest() {
+      let [oddArr, evenArr ] = lodash.partition([1, 2, 3, 4], n => n === 3);
+      console.log('three', oddArr);
+      console.log('rest', evenArr);
+
+      this.btnName = "You Clicked Me"
+    },
+    changeTitle(){
+      this.headingName = "New Heading Name"
+      this.myClass = "btn-green"
+    }
+  },
 }
 </script>
 
@@ -25,4 +54,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.btn-red{
+  color: red;
+}
+.btn-green{
+  color: green;
+}
+
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,16 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <div :class="headerClass">
+      <h1>{{ msg }}</h1>
+      <h1>{{ internalMsg }}</h1>
+      <h1>{{ computedMsg }}</h1>
+    </div>
+    <input type="text" v-model="fullName" />
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
+      For a guide and recipes on how to configure / customize this project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
@@ -34,9 +18,36 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      internalMsg: 'Testo',
+      lastName: 'Mark',
+      firstName: 'David',
+    };
+  },
+  computed: {
+    headerClass() {
+      return this.fullName.split(' ').length >  2 ? 'warning' : 'good';
+    },
+    computedMsg() {
+      return this.internalMsg + ' ' + this.msg;
+    },
+    fullName: {
+      // getter
+      get: function() {
+        return this.firstName + ' ' + this.lastName;
+      },
+      // setter
+      set: function(newValue) {
+        var names = newValue.split(' ');
+        this.firstName = names[0];
+        this.lastName = names[names.length - 1];
+      },
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -54,5 +65,20 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.warning {
+  background-color: tomato;
+  color: white;
+  border: 2px solid black;
+  margin: 20px;
+  padding: 20px;
+}
+.good {
+  background-color: #42b983;
+  color: white;
+  border: 2px solid black;
+  margin: 20px;
+  padding: 20px;
 }
 </style>
